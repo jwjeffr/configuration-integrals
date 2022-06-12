28ed8629c6f9a660ff87cbceaabe9802f0712f19
jwjeffr <88559006+jwjeffr@users.noreply.github.com>
2022-06-12 14:04:34 -0400

    Update default_interactions.py

--- default_interactions.py
+++ default_interactions.py
@@ -13,8 +13,8 @@ def lj_interaction_homogeneous(first_atom, second_atom, interaction_params):
 
 def lj_interaction_mixed(first_atom, second_atom, interaction_params):
 
-    sigma = interaction_params[f'sigma-{first_atom.atom_type}-{second_atom.type}']
-    dispersion = interaction_params[f'dispersion-{first_atom.atom_type}-{second_atom.type}']
+    sigma = interaction_params[f'sigma-{first_atom.atom_type}-{second_atom.atom_type}']
+    dispersion = interaction_params[f'dispersion-{first_atom.atom_type}-{second_atom.atom_type}']
     distance = np.linalg.norm(first_atom.position - second_atom.position)
 
     return 4 * dispersion * ((sigma / distance) ** 12 - (sigma / distance) ** 6)
